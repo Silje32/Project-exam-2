ae7151ca401a08774b0c8f8f7976de306c738d57
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-13 11:35:37 +0100

    Added h2, h3 and h4 to my styling

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -13,6 +13,31 @@ const GlobalStyles = createGlobalStyle`
         color: black
         flex-grow: 1;
     }
+
+    h2 {
+        color: #306FB6;
+        text-align: center;
+        font-size: 36px;
+        font-family: haboro-serif, serif;
+        font-style: normal;
+        font-weight: 500;
+    }
+
+    h3 {
+        color: #306FB6;
+        font-size: 26px;
+        font-family: haboro-serif, serif;
+        font-style: normal;
+        font-weight: 500;
+    }
+
+    h4 {
+        color: #306FB6;
+        font-size: 21px;
+        font-family: haboro-serif, serif;
+        font-style: normal;
+        font-weight: 400;
+    }
 `;
 
 export default GlobalStyles;
