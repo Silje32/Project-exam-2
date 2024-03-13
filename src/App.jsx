ef58d7862728a570ac85222f5aadf51d2263cff3
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-13 11:39:02 +0100

    Added another route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Comments from "./pages/Comments";
 import CreatePost from "./pages/CreatePost";
 import MyProfile from "./pages/MyProfile";
 import Layout from "./components/Layout/index";
@@ -16,8 +17,9 @@ import GlobalStyles from "./styles/GlobalStyles";
         <Routes>
           <Route path="/" element={<Layout />} >
             <Route index element={<Login />} />
-            <Route path="home" element={<Home />} />
             <Route path="register" element={<Register />} />
+            <Route path="home" element={<Home />} />
+            <Route path="comments/:id" element={<Comments />} />
             <Route path="create" element={<CreatePost />} />
             <Route path="my-profile" element={<MyProfile />} />
           </Route>
