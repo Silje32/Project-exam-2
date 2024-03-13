4cc2e2250b235404c4930cd67bcdd3ed879e2976
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-13 11:05:36 +0100

    Added an Images component

---
+++ src/components/Images/backgroundImage.jsx
@@ -0,0 +1,8 @@
+import Background from "../../images/modern3.jpg";
+import { StyledBackgroundImage } from "./backgroundImage.styles";
+
+function BackgroundImage() {
+    return <StyledBackgroundImage src={Background}  alt="Background Image" />
+}
+
+export default BackgroundImage;
---
+++ src/components/Images/backgroundImage.styles.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StyledBackgroundImage = styled.img`
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100%;
+`;
+
