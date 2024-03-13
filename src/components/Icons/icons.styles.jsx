bd3cc1c0c607afbb6464d583329b4d7d539fef4c
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-13 11:57:29 +0100

    Changed the color on my icon

--- src/components/Icons/icons.styles.jsx
+++ src/components/Icons/icons.styles.jsx
@@ -4,12 +4,11 @@ export const StyledIcons = styled.div`
    display: flex;
    align-items: center;
    justify-content: center;
-   display: inline-flex;
    width: 40px;
    height: 40px;
    justify-content: center;
    align-items: center;
    border-radius: 50%; 
    list-style-type: none;
-   color: yellow;
+   color: #FCE205;
 `;
