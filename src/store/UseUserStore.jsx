f1ad7c6068cd94a86b0f73dfca4afcd1b0217d4f
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-16 01:08:24 +0100

    Removed my actions key from the store

--- src/store/UseUserStore.jsx
+++ src/store/UseUserStore.jsx
@@ -5,19 +5,19 @@ const useUserStore = create(
     persist (
         (set) => ({
             user: null,
-            actions: {
-                setUser: (user) => set({ user: user }),
-                clearUser: () => set({ user: null }), 
-            },
+            setUser: (user) => set({ user: user }),
+            clearUser: () => set({ user: null }), 
         }),
-        { name: "user" }
+        {
+            name: "user", 
+        }
     )
 );
 
 export const useToken = () => useUserStore((state) => state.user?.accessToken);
 
 export const useUserActions = () => {
-   const { actions } = useUserStore();
-   return actions;
+   const { setUser, clearUser } = useUserStore();
+   return { setUser, clearUser };
 };
 
