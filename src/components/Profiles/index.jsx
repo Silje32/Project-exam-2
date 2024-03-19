950a49bdfa7d193d24534cfcd29d077bb523d8b2
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-19 11:06:09 +0100

    Added profiles to my project

---
+++ src/components/Profiles/index.jsx
@@ -0,0 +1,28 @@
+import { getprofilesurl } from "../../constants/api";
+import useApi from "../../hooks/useApi";
+
+function GetProfile () {
+
+    const { data: profile, isLoading, error  } = useApi(getprofilesurl);
+
+    if (isLoading) {
+      return <div>Loading posts...</div>;
+    }
+  
+    if (error) {
+      return <div>Error loading home page</div>;
+    }
+    
+    return ( 
+        <>
+          {profile.map((profile) => (
+             <div>
+                <h2>{profile.title}</h2>
+                <div>{profile.image}</div>
+             </div>
+          ))}
+        </>
+     );
+}
+
+export default GetProfile;
