48ee9549581ab118989f519f3d8bf2a7d9c55abb
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-20 23:06:20 +0100

    Removed my hook and did the API call the old fashion way

--- src/components/Profiles/getPosts.jsx
+++ src/components/Profiles/getPosts.jsx
@@ -1,15 +1,53 @@
+import { useState, useEffect } from 'react';
 import { postsurl } from "../../constants/api";
-import useApi from "../../hooks/useApi";
+
 
 
 function GetPost() {
-    const { data: posts, isLoading, error  } = useApi(postsurl);
+    const [posts, setPost] = useState([]);
+    const [isLoading, setisLoading] = useState(false);
+    const [isError, setisError] = useState(null);
+  
+    useEffect(() => {
+        async function getData() {
+          
+          const options = {
+            headers: { "Content-Type": "application/json" },
+            method: "GET",
+            body: JSON.stringify(data),  
+          };
+    
+
+          try {
+            setisLoading(true);
+            setisError(null);
+            const response = await fetch(postsurl, options);
+           
+            setPost(json);
+
+            if(!response.ok) {
+              const json = await response.json();
+              return json(posts);           
+        }
+
+
+          } catch (error) {
+            console.log(error);
+            setisError(true);
+          } finally {
+            setisLoading(false);    
+          } 
+        }    
+        
+       getData();
+    }, [postsurl]); 
+
 
     if (isLoading) {
       return <div>Loading posts...</div>;
     }
   
-    if (error) {
+    if (isError) {
       return <div>Error loading posts</div>;
     }
     
@@ -17,7 +55,7 @@ function GetPost() {
     return ( 
         <>
           {posts.map((post) => (
-             <div>
+             <div key={post.id}>
                 <h2>{post.title}</h2>
                 <p>{post.body}</p>
              </div>
