74ea8272b78bedc49b7473e2b5ffc79f1ac3e53f
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-25 10:21:43 +0100

    Removed my background image

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,13 +2,11 @@
 // A registered user may logout.
 
 import LoginCard from "../components/Cards/index";
-import BackgroundImage from "../components/Images/backgroundImage";
 
 function Login() {
     return (
         <>         
-          <LoginCard /> 
-          <BackgroundImage />    
+          <LoginCard />   
         </>
     );
 }
