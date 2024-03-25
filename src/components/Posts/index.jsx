36b0b361eaaef99bb3d9c562c5bd31227b44f830
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-25 10:52:04 +0100

    Removed my API hook

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,28 +1,62 @@
+import { useState, useEffect } from 'react';
 import { getpostsurl } from "../../constants/api";
-import useApi from "../../hooks/useApi";
 
 
 function PostList() {
-    const { data: posts, isLoading, error  } = useApi(getpostsurl);
 
-    if (isLoading) {
-      return <div>Loading posts...</div>;
-    }
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const [isError, setisError] = useState(false);
   
-    if (error) {
-      return <div>Error loading home page</div>;
-    }
+  
+      useEffect(() => {
+          async function getData(data) {
+              console.log(data);
+            
+            const options = {
+              headers: { "Content-Type": "application/json" },
+              method: "GET",
+              body: JSON.stringify(data),
+            };
+
+
+            try {
+              setisLoading(true);
+              setisError(false);
+
+              const response = await fetch(getpostsurl, options);
+              const json = await response.json();
+
+              setPosts(json);
+            } catch (error) {
+              console.log(error);
+              setisError(true);
+            } finally {
+              setisLoading(false);    
+            } 
+          } 
+          
+          
+         getData("getpostsurl");
+      }, []); 
     
+      if (isLoading || !posts) {
+        return <div>Loading posts...</div>;
+      }
+    
+      if (isError) {
+        return <div>Error loading posts</div>;
+      }
 
     return ( 
-        <>
+        <>  
           {posts.map((post) => (
-             <div>
-                <h2>{post.title}</h2>
-                <p>{post.body}</p>
-             </div>
-          ))}
-        </>
+            <div key={post.id}>        
+              <h2>title: {post.title}</h2>
+              <p>body: {post.body}</p>
+            </div>
+            ))}
+        </>            
     );
 }
 
