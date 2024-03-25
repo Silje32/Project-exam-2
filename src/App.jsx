8f6881814b2a4048efe65b45fbcac8f5a54e8f5c
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-25 01:56:34 +0100

    Updated my routes and made everything work

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import GlobalStyles from "./styles/GlobalStyles";
             <Route index element={<Login />} />
             <Route path="register" element={<Register />} />
             <Route path="home" element={<Home />} />
-            <Route path="comments/:id" element={<Comments />} />
+            <Route path="/home/comments/:id" element={<Comments />} />
             <Route path="create" element={<CreatePost />} />
             <Route path="my-profile" element={<MyProfile />} />
           </Route>
