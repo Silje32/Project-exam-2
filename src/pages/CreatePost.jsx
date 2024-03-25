3e063483ff5f2151b5e481315be5411701f86d07
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-25 10:38:51 +0100

    Removed my API hook

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,10 +1,12 @@
 // A registered user may create a Post.
 
 import CreatePostCard from "../components/Cards/createPostCard";
+import PublishButton from "../components/Buttons/publishButton";
 
 function CreatePost() {
     return (
         <>
+          <PublishButton />
           <CreatePostCard />
         </>
     );
