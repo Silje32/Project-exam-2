a70e266999b6217c8dab3418f5861111a887208e
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-26 10:19:21 +0100

    Removed my hook

--- src/components/Comments/index.jsx
+++ src/components/Comments/index.jsx
@@ -1,23 +1,58 @@
-import { postsidurl } from "../../constants/api";
-import useApi from "../../hooks/useApi";
+import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
-
+import { postsidurl } from "../../constants/api";
 
 function Comment() {
-    const { isLoading, error  } = useApi(postsidurl);
+
+    const [comments, setComments] = useState([]);
+    const [isLoading, setisLoading] = useState(false);
+    const [isError, setisError] = useState(false);
+
+    const { id  } = useParams();
+
+    useEffect(() => {
+        async function getData(data) {
+          console.log(data);
+          
+          const options = {
+            headers: { "Content-Type": "application/json" },
+            method: "POST",
+            body: JSON.stringify(data),
+          };
+
+
+          try {
+            setisLoading(true);
+            setisError(false);
+
+            const response = await fetch(postsidurl, options);
+            const json = await response.json();
+
+            setComments(json);
+          } catch (error) {
+            console.log(error);
+            setisError(true);
+          } finally {
+            setisLoading(false);    
+          } 
+        } 
+        
+        
+       getData("postsidurl/${id}");
+    }, [id]); 
 
     if (isLoading) {
       return <div>Loading comments...</div>;
     }
   
-    if (error) {
+    if (isError) {
       return <div>Error loading comments</div>;
     }
     
 
     return ( 
         <>
-        
+          <div>{comments.title}</div>
         </>
     );
 }
