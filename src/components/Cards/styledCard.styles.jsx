b7d5eb0ac2d26cc94f97fce5f8492390846219e3
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-26 10:52:39 +0100

    Added StyledHomeCard to my styles

--- src/components/Cards/styledCard.styles.jsx
+++ src/components/Cards/styledCard.styles.jsx
@@ -8,7 +8,16 @@ export const StyledCard = styled.div`
    border-radius: 5px;
    margin: 20px 130px;
    padding: 60px;
-   z-index: 10;
+`
+
+export const StyledHomeCard = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   background-color: white;
+   border-radius: 5px;
+   margin: 20px 130px;
+   padding: 60px;
 `
 
 export const StyledProfileCard = styled.div`
