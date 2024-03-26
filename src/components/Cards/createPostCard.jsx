d198f94c5bae6fc1522e097cce07be2e191450ed
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-26 10:30:28 +0100

    Updated my CreatePostCard function

--- src/components/Cards/createPostCard.jsx
+++ src/components/Cards/createPostCard.jsx
@@ -7,20 +7,21 @@ function CreatePostCard() {
     return (  
         <>
           <StyledCard>
-              <div>
-                <h2>CREATE A POST</h2>
-              </div> 
-              <div>
-                <h3>PROFILE NAME:</h3>
-              </div> 
-              <div>
-                <h4>What is on your mind today?</h4>
-                <p>Lorem Ipsum do la remi ta ta la santa tattaa.</p>
-                <p>Lorem Ipsum do la remi ta ta la santa tattaa.</p>
-                <p>Lorem Ipsum do la remi ta ta la santa tattaa.</p>
-                <StyledBaseButton>PUBLISH</StyledBaseButton>
+               <div>
+                  <h2>CREATE A POST</h2>
+               </div> 
+               <div>
+                  <h3>PROFILE NAME:</h3>
+               </div> 
+               <div>
+                  <h4>What is on your mind today?</h4>
+                  <p>Lorem Ipsum do la remi ta ta la santa tattaa.</p>
+                  <p>Lorem Ipsum do la remi ta ta la santa tattaa.</p>
+                  <p>Lorem Ipsum do la remi ta ta la santa tattaa.</p>
+               </div>
+               <div>
                 <StyledBaseButton>
-                    <Link to={"home"}>CLOSE</Link>
+                    <Link to={"/home"}>CLOSE</Link>
                 </StyledBaseButton>
               </div>
           </StyledCard>
