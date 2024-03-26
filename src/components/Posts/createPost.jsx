5d613646c61196506f1eb83850d5cd295c7cd03c
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-26 10:13:00 +0100

    Removed my hook

--- src/components/Posts/createPost.jsx
+++ src/components/Posts/createPost.jsx
@@ -1,23 +1,59 @@
+import { useState, useEffect } from 'react';
 import { postpostsurl } from "../../constants/api";
-import useApi from "../../hooks/useApi";
+
 
 
 function CreatePost() {
-    const { data: post, isLoading, error  } = useApi(postpostsurl);
+  const [create, setCreate] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const [isError, setisError] = useState(false);
+
+  useEffect(() => {
+    async function getData(data) {
+      console.log(data);
+      
+      const options = {
+        headers: { "Content-Type": "application/json" },
+        method: "POST",
+        body: JSON.stringify(data),
+      };
+
+
+      try {
+        setisLoading(true);
+        setisError(false);
+
+        const response = await fetch(postpostsurl, options);
+        const json = await response.json();
+
+        setCreate(json);
+      } catch (error) {
+        console.log(error);
+        setisError(true);
+      } finally {
+        setisLoading(false);    
+      } 
+    } 
+    
+    
+   getData("postpostsurl");
+}, []); 
+  
+   
 
     if (isLoading) {
       return <div>Loading posts...</div>;
     }
   
-    if (error) {
+    if (isError) {
       return <div>Error loading Create new Post page</div>;
     }
     
 
     return ( 
         <>
-          {post.map((create) => (
-             <div>
+          {create.map((create) => (
+             <div key={create.id}>
                 <h2>{create.title}</h2>
                 <p>{create.body}</p>
              </div>
