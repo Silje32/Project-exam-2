752838e71c389166dfe7744e576e57d5a5b10592
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-28 11:49:37 +0100

    Added StyledProfileFilterCard and StyledButtonCard to my styles

--- src/components/Cards/styledCard.styles.jsx
+++ src/components/Cards/styledCard.styles.jsx
@@ -26,7 +26,30 @@ export const StyledProfileCard = styled.div`
    align-items: center;
    background-color: white;
    border-radius: 5px;
-   margin: 20px 130px;
+   max-width: 800px;
+   margin-left: 20px;
+   padding: 20px;
+`
+
+export const StyledProfileFilterCard = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   background-color: pink;
+   border-radius: 5px;
+   max-width: 400px;
+   margin-right: 20px;
+   padding: 20px;
+
+`
+
+export const StyledButtonCard = styled.div`
+   display: flex;
+   flex: 1;
+   flex-direction: row;
+   align-items: center;
+   background-color: white;
+   border-radius: 5px;
    padding: 60px;
    text-align: left;
 `
