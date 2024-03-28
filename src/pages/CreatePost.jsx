b528a1f8ba4eb09b05717bfda92f1b1058a79bb1
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-28 11:38:53 +0100

    Updated the CreatePost page

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,12 +1,10 @@
 // A registered user may create a Post.
 
 import CreatePostCard from "../components/Cards/createPostCard";
-import PublishButton from "../components/Buttons/publishButton";
 
 function CreatePost() {
     return (
         <>
-          <PublishButton />
           <CreatePostCard />
         </>
     );
