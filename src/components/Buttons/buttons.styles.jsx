a3513aea3891b07248fed16bdabc65ab234fac27
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-29 13:15:28 +0100

    Added styles to my update button

--- src/components/Buttons/buttons.styles.jsx
+++ src/components/Buttons/buttons.styles.jsx
@@ -81,6 +81,35 @@ export const StyledCommentsButton = styled.button`
    }
 `  
 
+export const StyledUpdateButton = styled.button`
+@font-face {
+   font-family: 'noveldisplay-mediumxcmp';
+   src: url('noveldisplay-mediumxcmp-webfont.woff2') format('woff2'),
+        url('noveldisplay-mediumxcmp-webfont.woff') format('woff');
+   font-weight: normal;
+   font-style: normal;
+}
+   padding: 10px 20px;
+   display: flex;
+   flex: 3;
+   flex-direction: column;
+   justify-content: space-between;
+   font-size: 22px;
+   min-width: 150px;
+   cursor: pointer;
+   color: ${(props) => (props.$isActive ? "blue" : "white")};
+   background-color: ${(props) => (props.$isActive ? "pink" : "#707070")};
+
+   a {
+      text-decoration: none;
+      color: ${(props) => (props.$isActive ? "blue" : "white")};
+   }
+
+   a:hover {
+      background-color: "#C09DDF";
+   }
+`  
+
 export const StyledLikeButton = styled.button`
 @font-face {
    font-family: 'noveldisplay-mediumxcmp';
@@ -154,7 +183,7 @@ export const StyledDeleteButton = styled.button`
 }
    padding: 10px 20px;
    display: flex;
-   flex: 3;
+   flex: 4;
    flex-direction: row;
    justify-content: space-between;
    font-size: 22px;
