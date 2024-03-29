4a3515ec4450abb3c960c2c479af564b0b0c3244
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-29 12:57:44 +0100

    Updated my styles on my input

--- src/components/Searchbar/styledProfileFilter.styles.jsx
+++ src/components/Searchbar/styledProfileFilter.styles.jsx
@@ -1,14 +1,11 @@
 import styled from 'styled-components';
 
 export const StyledProfileFilter = styled.input`
-   display: flex;
-   flex-direction: column;
    border: 2px solid #5EA7EF;
-   background: none;
+   background: white;
    padding: 10px 20px;
-   margin-bottom: 20px;
    font-size: 18px;
-   min-width: 200px;
+   min-width: 500px;
    cursor: pointer;
    color: ${(props) => (props.$isActive ? "#306FB6" : "black")};
 
@@ -16,4 +13,4 @@ export const StyledProfileFilter = styled.input`
       border-color: #C09DDF;
    }    
 `
-
+;
