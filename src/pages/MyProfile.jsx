db25cd511ade02f398cc785f5d82c81e3898ebef
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-29 12:55:33 +0100

    Updated the My Profile page

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,24 +1,16 @@
-// A registered user may view a list of Profiles.
-// A registered user may update their avatar and banner.
-// A registered user may view a list of Posts.
-// A registered user may view a single Profile by name.
-// A registered user may react to any Post with an emoji.
-// A registered user may follow and unfollow another Profile.
-// A registered user may update a Post they own.
-// A registered user may delete a Post they own. 
-
 import Banner from "../components/Banner/index";
 import ProfileCard from "../components/Cards/profileCard";
-import ProfileFilterCard from "../components/Cards/profileFilterCard";
-import { StyledProfileFilterCard } from "../components/Cards/styledCard.styles";
+import ProfileInputCard from "../components/Cards/profileInputCard";
+import { StyledMyProfileCard } from "../components/Cards/styledCard.styles";
 
 function MyProfile() {
     return (
         <> 
-           <Banner />          
-           <ProfileCard />
-           <ProfileFilterCard />
-           <StyledProfileFilterCard />
+          <StyledMyProfileCard>
+             <Banner />          
+             <ProfileCard />
+             <ProfileInputCard />
+          </StyledMyProfileCard>
         </>      
     );
 }
