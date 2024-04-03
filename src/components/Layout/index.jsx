4e5769b7c735c31b145987edeea80f28591d54fd
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-03 14:00:07 +0200

    Moved my header component from the Layout component and into the nav

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Nav from "./nav";
-import Header from "../Header/index";
 import Footer from "../Footer/index";
 
 function Layout() {
     return (
       <>
         <Nav />
-        <Header />
         <Outlet />
         <Footer />
       </>
