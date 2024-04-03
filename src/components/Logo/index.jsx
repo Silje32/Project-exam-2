21e3afaf89388e43d9001c06cf6f87c48311b351
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-03 15:05:25 +0200

    Made my logo clickable

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,12 +1,19 @@
+import { Link } from "react-router-dom";
 import Logo from "../../images/Logo_final.jpg";
 import { StyledLogo } from "./logo.styles";
 
 
 function LogoImg() {
 
+    function onLogoClick() {
+
+    }
+
     return (
       <>
-        <StyledLogo src={Logo}  alt="Logo" />
+        <Link to={"/home"}>
+        <StyledLogo src={Logo}  alt="Logo" onClick={onLogoClick} />
+        </Link>  
       </>     
     );
   }
