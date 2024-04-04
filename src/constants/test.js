de1979356a9e69d37754cdc8ff9123d2efe33879
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-04 14:40:22 +0200

    Fixed a bug

--- src/constants/test.js
+++ src/constants/test.js
@@ -1,12 +1,12 @@
-import test from "./test";
 
 require("dotenv/config");
 
 function test(expectedSecret) {
   const secretMatches = process.env.API_URL == expectedSecret;
-  console.log(`The api url is ${process.env.MEANING_OF_LIFE}`);
+  console.log(`The api url is ${process.env.API_URL}`);
   console.log(`The secret does${secretMatches ? "" : " not"} match!`);
   return secretMatches;
 }
+
 
 test"(https://nf-api.onrender.com/api/v1/)"
