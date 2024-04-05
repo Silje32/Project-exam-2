d002b2b526fee9562b3e377a7003f3aaa23b733c
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-05 13:50:49 +0200

    Added a new route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Comments from "./pages/Comments";
+import SinglePost from "./pages/SinglePost";
 import CreatePost from "./pages/CreatePost";
 import MyProfile from "./pages/MyProfile";
 import Layout from "./components/Layout/index";
@@ -19,6 +20,7 @@ import GlobalStyles from "./styles/GlobalStyles";
             <Route index element={<Login />} />
             <Route path="register" element={<Register />} />
             <Route path="home" element={<Home />} />
+            <Route path="/home/:id" element={<SinglePost />} />
             <Route path="/home/comments/:id" element={<Comments />} />
             <Route path="create" element={<CreatePost />} />
             <Route path="my-profile" element={<MyProfile />} />
