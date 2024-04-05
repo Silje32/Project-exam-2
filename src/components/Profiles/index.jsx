96ace328df207f0e0770e781f14e0802cc22257e
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-05 13:37:01 +0200

    Added token to my GetProfile function

--- src/components/Profiles/index.jsx
+++ src/components/Profiles/index.jsx
@@ -1,16 +1,23 @@
-import { useState, useEffect } from 'react';
-import { getprofilesurl } from "../../constants/api";
+// A registered user may view a list of Profiles
 
+import { useState, useEffect } from 'react';
+import { NEW_URL } from "../../constants/api";
+import { useToken } from "../../store/UseUserStore";
 
 function GetProfile () {
+
+  const token = useToken();
+
+  console.log("token", token);
+
     const [profile, setProfile] = useState([]);
     const [isLoading, setisLoading] = useState(false);
     const [isError, setisError] = useState(false);
   
     
       useEffect(() => {
-          async function getData(data) {
-            console.log(data);
+          async function getData(profile) {
+            console.log(profile);
             
             const options = {
               headers: { "Content-Type": "application/json" },
@@ -21,8 +28,8 @@ function GetProfile () {
             try {
               setisLoading(true);
               setisError(null);
-              
-              const response = await fetch(getprofilesurl, options);
+
+              const response = await fetch(`${NEW_URL}social/profiles`, options);
 
               if(response.ok) {
                 const json = await response.json();
@@ -37,7 +44,7 @@ function GetProfile () {
             } 
           }    
           
-         getData("getprofilesurl");
+         getData();
       }, []); 
 
 
