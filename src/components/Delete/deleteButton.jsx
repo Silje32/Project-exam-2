342f48da325d30a46ae7f5109207d7b43123b327
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-08 14:44:41 +0200

    Updated and moved my delete button to its own folder

---
+++ src/components/Delete/deleteButton.jsx
@@ -0,0 +1,69 @@
+// A registered user may delete a Post they own. 
+
+import { useState } from 'react';
+import { NEW_URL } from "../../constants/api";
+import { StyledDeleteButton } from "../Buttons/buttons.styles";
+
+
+function DeleteButton() {
+  const [data, setData] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const [isError, setisError] = useState(false);
+
+          
+      async function onSubmit(data) {
+          console.log(data);
+      
+      const options = {
+        headers: { "Content-Type": "application/json" },
+        method: "DELETE",
+        body: JSON.stringify(data),
+      };
+
+
+      try {
+        setisLoading(true);
+        setisError(false);
+
+        const response = await fetch(`${NEW_URL}social/posts/{id}/comment/{commentId}`, options);
+        const json = await response.json();
+
+        setData(json);
+      } catch (error) {
+        console.log(error);
+        setisError(true);
+      } finally {
+        setisLoading(false);    
+      } 
+    } 
+
+
+    if (isLoading) {
+      return <div>Delete this post...</div>;
+    }
+  
+    if (isError) {
+      return <div>Deleting this post was not possible</div>;
+    }
+
+    function onButtonClick() {
+      console.log("Button clicked");
+
+      if (id) {
+        
+      }
+            
+    }
+
+
+    return (
+      <>
+        <StyledDeleteButton onClick={onButtonClick}>
+           DELETE
+        </StyledDeleteButton>
+      </> 
+      
+    );
+}
+
+export default DeleteButton;
---
+++ src/components/Delete/styledDeleteButton.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const StyledDeleteButton = styled.button`
+@font-face {
+   font-family: 'noveldisplay-mediumxcmp';
+   src: url('noveldisplay-mediumxcmp-webfont.woff2') format('woff2'),
+        url('noveldisplay-mediumxcmp-webfont.woff') format('woff');
+   font-weight: normal;
+   font-style: normal;
+}
+   padding: 10px 20px;
+   display: flex;
+   flex: 4;
+   flex-direction: row;
+   font-size: 22px;
+   min-width: 150px;
+   cursor: pointer;
+   color: ${(props) => (props.$isActive ? "blue" : "white")};
+   background-color: ${(props) => (props.$isActive ? "#707070" : "black")};
+
+   &:hover {
+      background-color: "#C09DDF";
+   }
+
+   a {
+      text-decoration: none;
+      color: ${(props) => (props.$isActive ? "blue" : "white")};
+   }
+   
+`
