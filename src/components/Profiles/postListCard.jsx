ee4957705165a0c3751fd8ce87a563e5b46e6283
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-08 03:08:22 +0200

    Added a PostListCard to my project

---
+++ src/components/Profiles/postListCard.jsx
@@ -0,0 +1,30 @@
+import Icons from "../Icons/index";
+import LikeButton from "../Buttons/likeButton";
+import CommentsButton from "../Buttons/commentsButton";
+import UpdateButton from "../Buttons/updateButton";
+import DeleteButton from "../Buttons/deleteButton";
+import { StyledButtonCard } from "../Cards/styledCard.styles";
+import { StyledPostListCard } from "./postListCard.styles";
+
+export function PostListCard({ post: { title, body, image } }) {
+
+    return ( 
+        <>
+         <StyledPostListCard>
+            <h3>Title: {title}</h3>
+            <p>{body}</p>
+            <img src={`${image}`} alt="" />
+            <StyledButtonCard>
+                <Icons />
+                <LikeButton />
+                <CommentsButton />
+                <UpdateButton />
+                <DeleteButton />
+            </StyledButtonCard>
+         </StyledPostListCard>
+        </>
+
+    );
+}
+
+export default PostListCard;
