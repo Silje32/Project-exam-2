9913997f0f7291507d11f82d79a8ea4c93af83c3
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-08 15:06:50 +0200

    Renamed my file

---
+++ src/components/Banner/banner.jsx
@@ -0,0 +1,59 @@
+// A registered user may update their avatar and banner.
+
+import { useState, useEffect } from 'react';
+import { NEW_URL } from "../../constants/api";
+
+
+function Banner() {
+  const [banner, setMedia] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const [isError, setisError] = useState(false);
+
+  useEffect(() => {
+    async function getData(data) {
+      console.log(data);
+      
+      const options = {
+        headers: { "Content-Type": "application/json" },
+        method: "PUT",
+      };
+
+
+      try {
+        setisLoading(true);
+        setisError(false);
+
+        const response = await fetch(`${NEW_URL}social/profiles/{name}/media`, options);
+        const json = await response.json();
+
+        setMedia(json);
+      } catch (error) {
+        console.log(error);
+        setisError(true);
+      } finally {
+        setisLoading(false);    
+      } 
+    } 
+        
+   getData();
+}, []); 
+
+
+    if (isLoading) {
+      return <div>Loading banner...</div>;
+    }
+  
+    if (isError) {
+      return <div>Error loading banner</div>;
+    }
+    
+
+
+    return (
+      <> 
+        <h2>Banner:{banner.body}</h2>
+      </>
+    );
+}
+
+export default Banner;
