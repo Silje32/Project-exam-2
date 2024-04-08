65eebec69200051bec2924b35f5d5aa426ba20fa
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-08 22:21:06 +0200

    Added StyledFlex to My Profile page

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,16 +1,16 @@
 import Banner from "../components/Banner/banner";
 import ProfileCard from "../components/Cards/profileCard";
 import ProfileInputCard from "../components/Cards/profileInputCard";
-import { StyledMyProfileCard } from "../components/Cards/styledCard.styles";
+import { StyledFlex } from "../components/Cards/styledCard.styles";
 
 function MyProfile() {
     return (
         <> 
           <Banner />  
-          <StyledMyProfileCard>      
+          <StyledFlex>      
              <ProfileCard />           
              <ProfileInputCard />
-          </StyledMyProfileCard>
+          </StyledFlex>
         </>      
     );
 }
