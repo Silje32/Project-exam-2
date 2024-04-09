c79f889528f97ccf84a82265aec96684a8c8e5f1
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-09 02:27:36 +0200

    Updated my HomeCard function

--- src/components/Cards/homeCard.jsx
+++ src/components/Cards/homeCard.jsx
@@ -1,11 +1,7 @@
-import Icons from "../Icons/index";
-import Emoji from "../Emoji/emoji";
+import GetAllProfiles from "../Profiles/getAllProfiles";
 import GetPost from "../Profiles/getPosts";
-import GetProfile from "../Profiles/getProfile";
-import LikeButton from "../Buttons/likeButton";
-import CommentsButton from "../Buttons/commentsButton";
 import ViewPostButton from "../Buttons/viewPostButton";
-import { StyledHomeCard, StyledButtonCard } from "../Cards/styledCard.styles";
+import { StyledHomeCard } from "../Cards/styledCard.styles";
 
 
 
@@ -15,20 +11,14 @@ function HomeCard() {
         <>
           <StyledHomeCard>      
               <div>
-              <GetProfile />
+              <GetAllProfiles />
               </div> 
               <div>
               <GetPost />
               </div>
-                <div>
-                <Icons />
-                <Emoji />
-                <StyledButtonCard>
-                  <LikeButton />
-                  <CommentsButton />
-                </StyledButtonCard>   
-              </div>
-              <ViewPostButton />
+              <div>
+                <ViewPostButton />       
+              </div>   
           </StyledHomeCard>
         </>
     );
