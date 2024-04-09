8c2a11c690858b7dc39761cc46dcf182511dd281
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-09 14:27:15 +0200

    Added margin-top to my styling

--- src/components/Icons/icons.styles.jsx
+++ src/components/Icons/icons.styles.jsx
@@ -2,13 +2,15 @@ import styled from "styled-components";
 
 export const StyledIcons = styled.div`
    display: flex;
-   align-items: center;
+   align-items: left;
    justify-content: center;
    width: 40px;
    height: 40px;
    justify-content: center;
+   padding: 10px;
    align-items: center;
    border-radius: 50%; 
    list-style-type: none;
    color: #FCE205;
+   margin-top: 500px;
 `;
