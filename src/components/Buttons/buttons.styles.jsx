4de470ee2428162b8b302115cc88520b203d1ba3
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-09 23:54:14 +0200

    Removed my StyledDeleteButton

--- src/components/Buttons/buttons.styles.jsx
+++ src/components/Buttons/buttons.styles.jsx
@@ -60,13 +60,13 @@ export const StyledCommentsButton = styled.button`
    font-weight: normal;
    font-style: normal;
 }
-   padding: 10px 20px;
+   padding: 12px 25px;
    display: flex;
    flex: 2;
    flex-direction: column;
-   justify-content: space-between;
-   font-size: 22px;
+   font-size: 20px;
    min-width: 150px;
+   margin-right: 10px;
    cursor: pointer;
    color: ${(props) => (props.$isActive ? "blue" : "white")};
    background-color: ${(props) => (props.$isActive ? "pink" : "#707070")};
@@ -93,8 +93,8 @@ export const StyledUpdateButton = styled.button`
    display: flex;
    flex: 3;
    flex-direction: column;
-   justify-content: space-between;
    font-size: 22px;
+   margin-right: 10px;
    min-width: 150px;
    cursor: pointer;
    color: ${(props) => (props.$isActive ? "blue" : "white")};
@@ -122,9 +122,9 @@ export const StyledLikeButton = styled.button`
    display: flex;
    flex: 1;
    flex-direction: row;
-   justify-content: space-between;
    font-size: 22px;
    min-width: 150px;
+   margin-right: 10px;
    cursor: pointer;
    color: ${(props) => (props.$isActive ? "blue" : "white")};
    background-color: ${(props) => (props.$isActive ? "black" : "#707070")};
@@ -158,6 +158,7 @@ export const StyledLikesButton = styled.button`
    min-width: 150px;
    cursor: pointer;
    border-color: #5EA7EF;
+   margin-right: 20px;
    color: ${(props) => (props.$isActive ? "blue" : "black")};
    background-color: ${(props) => (props.$isActive ? "black" : "white")};
 
@@ -185,20 +186,19 @@ export const StyledDeleteButton = styled.button`
    display: flex;
    flex: 4;
    flex-direction: row;
-   justify-content: space-between;
    font-size: 22px;
    min-width: 150px;
    cursor: pointer;
    color: ${(props) => (props.$isActive ? "blue" : "white")};
    background-color: ${(props) => (props.$isActive ? "#707070" : "black")};
 
+   &:hover {
+      background-color: "#C09DDF";
+   }
+
    a {
       text-decoration: none;
       color: ${(props) => (props.$isActive ? "blue" : "white")};
-   }
-
-   a:hover {
-      background-color: "#C09DDF";
    }
    
 `
@@ -213,6 +213,7 @@ export const StyledPublishButton = styled.button`
    padding: 10px 20px;
    font-size: 22px;
    min-width: 200px;
+   margin-right: 20px;
    cursor: pointer;
    color: ${(props) => (props.$isActive ? "blue" : "white")};
    background-color: ${(props) => (props.$isActive ? "#707070" : "black")};
