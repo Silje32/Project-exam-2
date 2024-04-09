8c8f0ee4028477fee94478bc7ed93164b5a59a9d
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-09 14:02:04 +0200

    Renamed my Banner function and styled it

---
+++ src/components/Media/mediaList.jsx
@@ -0,0 +1,13 @@
+import { StyledBanner, StyledAvatar } from "./mediaList.styles";
+
+export function Media() {
+
+    return ( 
+        <>
+          <StyledBanner src={"https://url.com/image.jpg"}  alt="Banner"  />
+          <StyledAvatar src={"https://url.com/image.jpg"}  alt="Avatar"  />
+        </>
+    );
+}
+
+export default Media;
---
+++ src/components/Media/mediaList.styles.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledBanner = styled.img`
+
+img {
+    padding: 10px;
+    width: 100%;
+}
+
+`
+
+export const StyledAvatar = styled.img`
+
+img {
+    padding: 10px;
+    width: 50%;
+}
+
+`
+;
+
