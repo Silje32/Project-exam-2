c9686cb8bf94b1923bd4767742fcbd008fd27f97
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-09 03:12:47 +0200

    Added GetAllProfileList function to the project and styled it

---
+++ src/components/Profiles/getAllProfileList.jsx
@@ -0,0 +1,20 @@
+import { StyledGetAllProfileList } from "./getAllProfileList.styles";
+
+export function GetAllProfileList({ profile: { banner, avatar, name, email, _count  } }) {
+
+    return ( 
+        <>
+         <StyledGetAllProfileList>   
+            <img src={`${banner}`} alt="Banner" />
+            <img src={`${avatar}`} alt="Avatar" />
+            <h2>Profile name: {name}</h2>
+            <p>Email: {email}</p>
+            <p>Posts: {_count.posts}</p>
+            <p>Followers: {_count.followers}</p>
+            <p>Following: {_count.following}</p>
+         </StyledGetAllProfileList>
+        </>
+    );
+}
+
+export default GetAllProfileList;
