299eb5b4ca2092c8b1db92575539da2a6f0cc3ac
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-11 01:34:49 +0200

    Updated my searchbar function

--- src/components/Searchbar/profileFilterInput.jsx
+++ src/components/Searchbar/profileFilterInput.jsx
@@ -1,65 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import ProfileFilter  from "./profileFilter";
-import { NEW_URL } from "../../constants/api";
-import { useToken } from "../../store/UseUserStore";
+
 
 function ProfileFilterInput() {
 
-   const token = useToken();
-
-   console.log("token", token);
-
-
    const [searchTerm, setSearchTerm] = useState([]);
-   const [isLoading, setisLoading] = useState(null);
-   const [isError, setisError] = useState(null);
     
    console.log("search", searchTerm );
 
-   useEffect(() => {
-    async function getData(data) {
-      console.log(data);
-      
-      const options = {
-        headers: {
-           "Content-Type": "application/json",
-           Authorization: `Bearer ${token}`, 
-      },
-        method: "GET",
-      };
-
-
-      try {
-        setisLoading(true);
-        setisError(false);
-        const response = await fetch(`${NEW_URL}social/profiles/{name}`, options);
-
-        if (response.ok) {
-          const json = await response.json();
-          return setSearchTerm(json);           
-        }
-
-        throw new Error();
-      } catch (Error) {
-        console.log(Error);
-        setisError(true);
-      } finally {
-        setisLoading(false);    
-      } 
-    } 
-        
-   getData();
-}, []); 
-
-  if (isLoading) {
-    return <div>Loading profiles...</div>;
-  }
-
-  if (isError) {
-    return <div>Error loading profiles</div>;
-  }
-
-
+   
     return (  
         <>
            <ProfileFilter onChange={(event) => setSearchTerm(event.target.value.trim())}  />
