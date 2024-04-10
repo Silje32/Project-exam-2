786fe8799b1544ded3976a854a31bd2883ed057d
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-10 12:19:28 +0200

    Added SingleProfile page to my project

---
+++ src/pages/SingleProfile.jsx
@@ -0,0 +1,12 @@
+import SingleProfileCard from "../components/Cards/singleProfileCard";
+
+function SingleProfile() {
+
+    return (
+       <>
+          <SingleProfileCard />
+       </>
+    );       
+}
+
+export default SingleProfile;  
