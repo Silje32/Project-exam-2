ede6cb761481410dd5fc55fee5ef941c9f298ea3
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-11 13:15:40 +0200

    Updated my Emoji function

--- src/components/Emoji/emoji.jsx
+++ src/components/Emoji/emoji.jsx
@@ -16,7 +16,7 @@ function Emoji() {
       const options = {
         headers: { "Content-Type": "application/json" },
         method: "PUT",
-        body: JSON.stringify( symbol ),
+        body: JSON.stringify(),
       };
 
       try {
