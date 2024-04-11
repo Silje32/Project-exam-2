6ef07f76219c9588d1fc0b4aeb77173be497c135
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-11 14:41:15 +0200

    Changed my url src

--- src/components/Media/mediaList.jsx
+++ src/components/Media/mediaList.jsx
@@ -4,8 +4,8 @@ export function Media() {
 
     return ( 
         <>
-          <StyledBanner src={"https://url.com/image.jpg"}  alt="Banner"  />
-          <StyledAvatar src={"https://url.com/image.jpg"}  alt="Avatar"  />
+          <StyledBanner src={`https://url.com/image.jpg`}  alt="Banner"  />
+          <StyledAvatar src={`https://url.com/image.jpg`}  alt="Avatar"  />
         </>
     );
 }
