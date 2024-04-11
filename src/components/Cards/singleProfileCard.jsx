a1a731c8fcda9b394f8ae6dc7ac1fcdf321dce76
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-11 22:56:28 +0200

    Added BackToProfileButton to my SingleProfileCard

--- src/components/Cards/singleProfileCard.jsx
+++ src/components/Cards/singleProfileCard.jsx
@@ -1,7 +1,7 @@
 import Icons from "../Icons/icons";
 import Emoji from "../Emoji/emoji";
 import LikeButton from "../Buttons/likeButton";
-import CloseButton from "../Buttons/backButton";
+import BackToProfileButton from "../Buttons/backToProfileButton";
 import SingleProfile from "../SingleProfile/singleProfile";
 import { StyledSingleProfileCard, StyledButtonCard } from "../Cards/styledCard.styles";
 
@@ -15,7 +15,7 @@ function SingleProfileCard() {
                 <h3>Single Profile</h3>
               </div> 
               <div>
-              <CloseButton />
+              <BackToProfileButton/>
               </div>
               <div>
                 <SingleProfile />
