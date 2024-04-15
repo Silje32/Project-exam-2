cb642b1f38e44a9fddcedac9d2e747e36b8818ed
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-15 13:34:58 +0200

    Renamed my header and changed the name of my project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import { StyledHeading } from "./heading.styles";
+
+
+function Header() {
+
+    return (
+      <>
+        <StyledHeading> 
+             DROPS OF JOY
+        </StyledHeading> 
+      </>     
+    );
+  }
+
+export default Header;
