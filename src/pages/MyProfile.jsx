df48fa30f83fbee06514ee2516bdf33937772946
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-15 22:57:00 +0200

    Removed my StyledFlex and ProfileInputCard from MyProfile page

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,19 +1,15 @@
 import Media from "../components/Media/media";
 import ProfileCard from "../components/Cards/profileCard";
-import ProfileInputCard from "../components/Cards/profileInputCard";
-import { StyledFlex } from "../components/Cards/styledCard.styles";
+
 
 function MyProfile() {
-    return (
-        <> 
-          <Media />  
-          <StyledFlex>   
-             <ProfileCard />           
-             <ProfileInputCard />
-          </StyledFlex>
-        </>      
-    );
+
+  return (
+     <>
+        <Media />
+        <ProfileCard />
+     </>
+  );       
 }
 
 export default MyProfile;
-
