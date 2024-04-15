cac79da98c529a78c968f37deafd495fa9096581
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-15 14:07:55 +0200

    Updated my styles

---
+++ src/components/Posts/PostListCard.styles.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const StyledPostListCard = styled.div`
+display: flex;
+flex-direction: column;
+align-items: right;
+background-color: #306FB6;
+opacity: 0.8;
+border-radius: 5px;
+max-width: 865px;
+margin-right: 350px;
+margin-bottom: 30px;
+padding: 10px;
+
+img {
+    width: 80%;
+    background-color: white;
+    margin-bottom: 40px 30px;
+    margin-left: 85px;
+    padding: 20px;
+}
+
+h3 {
+    color: white;
+    padding: 10px;
+}
+
+p {
+    color: white;
+    font-size: 16px;
+    line-height: 1.5;
+    padding: 10px;
+}
+`;
