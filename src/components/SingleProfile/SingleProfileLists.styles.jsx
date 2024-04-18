7d7a09636b294b9b1e2c07740819fa51462cd3b5
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-18 10:30:06 +0200

    Added SingleProfileList to my project and styled it

---
+++ src/components/SingleProfile/SingleProfileLists.styles.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const StyledSingleProfileList = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+background-color: #306FB6;
+opacity: 0.8;
+border-radius: 5px;
+border-color: black;
+width: 1000px;
+max-width: 100%;
+margin: 20px 30px;
+padding: 10px;
+
+img {
+    width: 30%;
+    background-color: #306FB6;
+    padding: 20px;
+    border-radius: 50%;
+
+}
+
+h2 {
+    color: white;
+    text-align: center;
+    margin-top: 20px;
+}
+
+p {
+    color: white;
+    line-height: 1.5;
+    font-size: 18px;
+}
+
+`;
