da0d3213074ed3e4c343fb61edbfd5ad1d226d48
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-18 12:28:54 +0200

    Added ProfileFilter function to my project

---
+++ src/components/Searchbar/ProfilesFilter.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { StyledProfileFilter } from "./StyledProfileFilter.styles";
+
+
+function ProfileFilter({ posts = [] }) {
+  console.log("props", posts);
+
+  const [searchTerm, setSearchTerm] = useState("");
+    console.log("search", searchTerm);
+
+  
+  const filterPosts = posts.filter((post) => post.title.toLowerCase().includes(searchTerm.toLowerCase()));
+    console.log("filterPosts", filterPosts);
+
+
+    return ( 
+        <>
+          <StyledProfileFilter value={searchTerm} onChange={(event) => setSearchTerm(event.target.value.trim())}  />               
+        </>
+    );
+}
+
+export default ProfileFilter;
