ca69b716d358ec6b5c4a0d35de62eaaafefa0c37
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-20 00:00:26 +0200

    Updated my UpdateButton

--- src/components/Buttons/updateButton.jsx
+++ src/components/Buttons/updateButton.jsx
@@ -5,14 +5,61 @@ import { StyledUpdateButton } from "./Button.styles";
 
 function UpdateButton() {
 
+  const [update, setUpdate] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const [isError, setisError] = useState(false);
+
+
+    async function onSubmit(update) {
+        console.log(update);
+      
+      const options = {
+        headers: { "Content-Type": "application/json" },
+        method: "PUT",
+        body: JSON.stringify(),
+      };
+
+
+      try {
+        setisLoading(true);
+        setisError(false);
+
+        const response = await fetch(`${NEW_URL}/social/posts/${id}`, options);
+        const json = await response.json();
+
+        setFollow(json);
+      } catch (error) {
+        console.log(error);
+        setisError(true);
+      } finally {
+        setisLoading(false);    
+      } 
+
+      getData();
+    }, [id]); 
+    
+
+        if (isLoading) {
+           return <h3>Follow profile</h3>;
+        }
+
+  
+        if (isError) {
+           return <div>An error occured when following this profile</div>;
+        }
+    
+
     function onButtonClick() {
 
     }
 
     return ( 
-        <StyledUpdateButton onClick={onButtonClick}>
+
+        {update.map((updates) => (
+            <div key={updates.id}>
+          <StyledUpdateButton onClick={onButtonClick}>
             UPDATE
-        </StyledUpdateButton>
+          </StyledUpdateButton>
     );
 }
 
