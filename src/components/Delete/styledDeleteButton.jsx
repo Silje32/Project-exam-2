47343212988fbf19d9f7a8be436632e5661a9225
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-21 02:28:19 +0200

    Updated my styles

--- src/components/Delete/styledDeleteButton.jsx
+++ src/components/Delete/styledDeleteButton.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const StyledDeleteButton = styled.button`
+export const StyledDeletePostButton = styled.button`
 @font-face {
    font-family: 'noveldisplay-mediumxcmp';
    src: url('noveldisplay-mediumxcmp-webfont.woff2') format('woff2'),
@@ -28,3 +28,33 @@ export const StyledDeleteButton = styled.button`
    }
    
 `
+
+export const StyledDeleteCommentButton = styled.button`
+@font-face {
+   font-family: 'noveldisplay-mediumxcmp';
+   src: url('noveldisplay-mediumxcmp-webfont.woff2') format('woff2'),
+        url('noveldisplay-mediumxcmp-webfont.woff') format('woff');
+   font-weight: normal;
+   font-style: normal;
+}
+   padding: 10px 20px;
+   display: flex;
+   flex: 4;
+   flex-direction: row;
+   font-size: 22px;
+   width: 140px;
+   cursor: pointer;
+   color: ${(props) => (props.$isActive ? "blue" : "white")};
+   background-color: ${(props) => (props.$isActive ? "#707070" : "black")};
+
+   &:hover {
+      background-color: "#C09DDF";
+   }
+
+   a {
+      text-decoration: none;
+      color: ${(props) => (props.$isActive ? "blue" : "white")};
+   }
+   
+`;
+
