1ddeaf6970d97e6d382d9b2f68d42d75d7afed9d
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-22 13:29:41 +0200

    Updated my import

--- src/components/Cards/FollowingCard.jsx
+++ src/components/Cards/FollowingCard.jsx
@@ -1,6 +1,6 @@
 // Retrieve all posts from profiles that the authenticated user is following.
 
-import Following from "../Following/Following";
+import Following from "../Following/following";
 import { StyledFollowingCard, StyledFlex } from "./StyledCard.styles";
 
 
