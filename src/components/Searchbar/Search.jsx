5867b9c918001c925840da2f5ee14cc46756b5c4
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-23 10:42:38 +0200

    Added Search function to my project

---
+++ src/components/Searchbar/Search.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react';
+import ProfilesFilter from "./ProfilesFilter";
+import { NEW_URL } from "../../constants/api";
+
+
+function Search() {
+
+  const [search, setSearch] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const [isError, setisError] = useState(false);
+  
+  
+        useEffect(() => {
+          async function getData() {
+            
+            const options = {
+              headers: {
+                 "Content-Type": "application/json",
+              },
+              method: "GET",
+            };
+
+
+            try {
+              setisLoading(true);
+              setisError(false);
+
+              const response = await fetch(`${NEW_URL}/social/posts/search?q=<query>`,options);
+
+              if (response.ok) {
+                const json = await response.json();
+                return setSearch(json);
+              }
+              
+              throw new Error();
+            } catch (Error) {
+              console.log(Error);
+              setisError(true);
+            } finally {
+              setisLoading(false);    
+            } 
+          } 
+                    
+           getData();
+        }, []); 
+    
+        if (isLoading) {
+          return <div>Loading posts...</div>;
+        }
+    
+        if (isError) {
+          return <div>Error loading posts</div>;
+        }
+
+
+        return ( 
+         <>  
+           {search.map((data, id) => (
+             <ProfilesFilter key={id} data={data} />
+           ))}    
+         </>            
+        );
+}
+
+export default Search;
